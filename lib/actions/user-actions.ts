32c9f15bf7679bddb9834fd1b3c7ba20fcba9073
Rakib Hasan <rakibhasanroky0@gmail.com>
2025-04-11 21:49:10 +0600

    feat: Implement user management actions for create, update, and delete operations

--- lib/actions/user-actions.ts
+++ lib/actions/user-actions.ts
@@ -2,13 +2,53 @@
 
 import User from "@/database/user.model";
 import dbConnect from "../mongoose";
-import { CreateUserParams } from "@/types/action";
+import {
+  clerkUserUpdateParams,
+  CreateUserParams,
+  DeleteUserParams,
+} from "@/types/action";
 
 export const createUser = async (userData: CreateUserParams) => {
   try {
     await dbConnect();
     const newUser = await User.create(userData);
-    return newUser;
+    return { success: true, data: JSON.parse(JSON.stringify(newUser)) };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const updateUser = async (params: clerkUserUpdateParams) => {
+  try {
+    await dbConnect();
+
+    const { clerkId, updateData } = params;
+
+    const udpatedUser = await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+    return { success: true, data: JSON.parse(JSON.stringify(udpatedUser)) };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const deleteUser = async (params: DeleteUserParams) => {
+  try {
+    await dbConnect();
+
+    const { clerkId } = params;
+    const user = await User.findOneAndDelete({ clerkId });
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // delete user
+    const deletedUser = await User.findByIdAndDelete(user._id);
+
+    return { success: true, data: JSON.parse(JSON.stringify(deletedUser)) };
   } catch (error) {
     console.log(error);
     throw error;
