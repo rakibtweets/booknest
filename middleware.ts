9238cdc132dd5c6de7c9910d211f37d02277a886
Rakib Hasan <rakibhasanroky0@gmail.com>
2025-04-11 23:36:40 +0600

    refactor: update clerk webhook codes to new version

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import { checkRole } from "./lib/roles";
-
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard",
-  "/dashboard(.*)",
-  "/profile(.*)",
-  "/orders(.*)",
-  "/wishlist(.*)",
-]);
-
-const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 const isPublicRoute = createRouteMatcher([
   "/",
   "/api/webhook",
+  "/api/webhook(.*)",
   "/about",
   "/books",
   "/books/:id",
@@ -28,22 +17,6 @@ const isPublicRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) await auth.protect();
-
-  // const isAdmin = await checkRole("admin");
-  // // Protect admin routes
-  // if (isAdminRoute(req)) {
-  //   await auth.protect();
-  //   // @ts-ignore
-  //   // if (!isAdmin) {
-  //   //   return NextResponse.redirect(new URL("/sign-in", req.url));
-  //   // }
-  // }
-
-  // // Protect user routes
-  // if (!isProtectedRoute(req)) {
-  //   // await auth.protect();
-  //   return NextResponse.redirect(new URL("/sign-in", req.url));
-  // }
 });
 
 export const config = {
